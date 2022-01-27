de4abb4daee6b7403c658b862707ead75b795a88
Andrew Smith <diver.vlz@gmail.com>
2022-01-27 17:37:36 +0300

    add requirements.txt

--- FinFax/settings.py
+++ FinFax/settings.py
@@ -76,13 +76,23 @@ WSGI_APPLICATION = 'FinFax.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        # 'PASSWORD': 'password',
+        # 'HOST': 'localhost',
+        # 'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
